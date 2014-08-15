fda99e9dde924cfea23e5fafa30858c4646eb294
Jeremy Fox <foxjerem@gmail.com>
2014-08-15 11:04:03 +0100

    25 down 16 to go

--- questions.rb
+++ questions.rb
@@ -128,7 +128,7 @@ end
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
-	array.to_h
+	
 end
 
 # get all the letters used in an array of words and return
@@ -136,23 +136,29 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+	result = []
+	array.each { |element| element.each_char { |char| result.push(char)} }
+	result.sort.uniq
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+	hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+	hash.keys.inject(:+) + hash.values.inject(:+)
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+	string.gsub(/[A-Z]/, '')
 end
 
 # round up a float up and convert it to an Integer,
